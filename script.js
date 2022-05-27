2f56261fc214b2464a0d80c2a19c15577837a4da
BINODINI behera <99307989+Binodinibehera@users.noreply.github.com>
2022-05-27 18:52:34 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-// variables this is an variable
+x = 10, y = 20, c = 30;
+var fname = "swami",
+    lname = "ji";
+var roll = 101;
+var city = "America";
+document.write(x + '<hr>');
+
+document.write(x + c + "<hr>");
+document.write(fname);
+document.write(lname + "<hr>");
+document.write(roll + "<hr>");
+console.log('your city= ' + city + "<hr>");
+console.log(roll);
